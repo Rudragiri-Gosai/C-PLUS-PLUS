0b1b0895fa826f5613caaab5a6d47ba2c767ee85
Rudragiri-Gosai <dipakgosai199@gmail.com>
2023-09-11 16:10:45 +0530

    a

--- practical-3/p4.cpp
+++ practical-3/p4.cpp
@@ -1,26 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class student{
-	private:
-		string s_name;
-		int s_age;
-	public:
-		get_data()
-		{
-			cout<<"Enter your Good name :";
-			cin>>s_name;
-			cout<<"Enter your Age :";
-			cin>>s_age;
-		}
-		put_data()
-		{
-			cout<<"Your Good name is ::"<<s_name<<endl;
-			cout<<"Your Age is       ::"<<s_age<<endl;
-		}
-}s1;
+class student
+{
+private:
+	string s_name;
+	int s_age;
+
+public:
+	void get_data()
+	{
+		cout << "Enter your Good name :";
+		cin >> s_name;
+		cout << "Enter your Age :";
+		cin >> s_age;
+	}
+	void put_data()
+	{
+		cout << "Your Good name is ::" << s_name << endl;
+		cout << "Your Age is       ::" << s_age << endl;
+	}
+} s1;
 int main()
 {
-	s1.get_data();	
+	s1.get_data();
 	s1.put_data();
 	return 0;
 }
