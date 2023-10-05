e25e29a2f850143eb8e5d585ac6ffe05dc7e1c15
Rudragiri-Gosai <dipakgosai199@gmail.com>
2023-10-05 09:24:28 +0530

    a

--- practical-4/p2.cpp
+++ practical-4/p2.cpp
@@ -3,36 +3,35 @@ using namespace std;
 class Employee
 {
 public:
-    string e_name;
-    int e_ID;
-    void get_data()
+    static string e_name;
+    static int e_ID;
+    static void get_data()
     {
-        cout << "Enter your Employee ID:";
+    	cout << "Enter your Employee ID:";
         cin >> e_ID;
         cout << "Enter your Employee name     :";
         cin.ignore();
-        getline(cin, e_name);
+        getline(cin,e_name);    
     }
-    void put_data()
+    static void put_data()
     {
-        cout << "Your Employee name :" << e_name << endl;
+        cout << "Your Employee name :"<<e_name<< endl;
         cout << "Your Employee ID :" << e_ID << endl;
     }
 };
 
 int main()
 {
-    Employee emp[2];
+    Employee emp[10];
     int i;
-    for (i = 0; i < 2; i++)
+    for (i = 0; i < 10; i++)
     {
         emp[i].get_data();
+    }
+    for (i = 0; i < 10; i++)
+    {
         emp[i].put_data();
     }
-    // for (i = 0; i < 10; i++)
-    // {
-    //     emp[i].put_data();
-    // }
 
     return 0;
 }
